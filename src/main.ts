f19c82b8143ceb5ce937ea2de804d815fd44e0ee
qliquiz <artgor005@gmail.vom>
2024-06-14 16:41:48 +0800

    swag

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Список задач')
+    .setDescription('Документация REST API')
+    .setVersion('1.0.0')
+    .addTag('qliquiz')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
+  
   await app.listen(process.env.PORT, () => console.log(`The server is running on port ${process.env.PORT}`));
 }
 
